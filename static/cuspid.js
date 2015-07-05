7115600e0e03ba34950f101b1fd8d74c6f23a874
Jason Plumb <jason@noisybox.net>
2015-07-05 00:41:04 -0700

    Dyncamic image load and blit to canvas

--- static/cuspid.js
+++ static/cuspid.js
@@ -4,11 +4,27 @@ function cuspidLoad(){
 	window.onresize = function(event){
 		resizeCanvasToWindow();
 	}
+	loadImages();
 }
 
 function resizeCanvasToWindow(){
-	var canvas = document.getElementById('cnv');
+	var canvas = $('#cnv').get(0);
 	var context = canvas.getContext('2d');
 	context.canvas.width = window.innerWidth - 10;
 	context.canvas.height = window.innerHeight - 10;
+}
+
+function imageLoaded(id){
+	blitFull(id);
+}
+
+function loadImages(){
+	$('#imagepool').append("<img src='/static/cuspid.jpg' id='cuspid' onload='imageLoaded(\"cuspid\")'/>");
+}
+
+function blitFull(id){
+	var canvas = $('#cnv').get(0);
+    var context = canvas.getContext('2d');
+    var img = $('#' + id).get(0);
+    context.drawImage(img, 0, 0, img.width, img.height, 0, 0, context.canvas.width, context.canvas.height);
 }
