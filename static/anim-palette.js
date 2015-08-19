3839fccc38ffdc460bc1cb8714b7ee6d771bbd42
Jason Plumb <jason@noisybox.net>
2015-08-18 20:47:01 -0700

    Simplify function return

--- static/anim-palette.js
+++ static/anim-palette.js
@@ -1,10 +1,8 @@
 
 //ok not really palette, more like a rolling intensity or something
 function rotatePalette( quad, direction, jerkiness ){
-	var fn = function _rotateFunction( timeMult ){
+	return function ( timeMult ){
 		var dir = (jerkiness/0xff) * ((direction==='DOWN') ? -1 : 1) * timeMult;
 		quad.material.uniforms['colorCycle'].value += dir;
 	}
-
-	return fn;
 }
