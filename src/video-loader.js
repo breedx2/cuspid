74c295185f579b78313687a97d833a3dc0ae35e8
jason <jason@noisybox.net>
2018-01-07 13:48:30 -0800

    add todo

--- src/video-loader.js
+++ src/video-loader.js
@@ -3,15 +3,15 @@
 const cache = {};
 
 class VideoLoader {
-  static load(url){
-		return new Promise( (fulfill, reject) => {
-			if(cache[url]){
-				console.log(`Image ${url} loaded from cache!`);
-				return fulfill(cache[url]);
-			}
-			const video = newVideo(url);
+  static load(url) {
+    return new Promise((fulfill, reject) => {
+      if (cache[url]) {
+        console.log(`Image ${url} loaded from cache!`);
+        return fulfill(cache[url]);
+      }
+      const video = newVideo(url);
       video.onplaying = function() {
-				console.log("Video loaded!");
+        console.log("Video loaded!");
         cache[url] = video;
         fulfill(video);
       };
@@ -19,18 +19,19 @@ class VideoLoader {
   }
 }
 
-function newVideo(url){
+function newVideo(url) {
   var video = document.createElement("video");
-	video.autoplay = true;
-	video.loop = true;
+  video.autoplay = true;
+  video.loop = true;
+  //TODO: Don't hard code these!
   video.width = 640;
-	video.height = 360;
-	video.muted = true;
+  video.height = 360;
+  video.muted = true;
   video.src = url;
-	video.url = url;
-  video.setAttribute( 'webkit-playsinline', 'webkit-playsinline' );
-	video.play();
-	return video;
+  video.url = url;
+  video.setAttribute('webkit-playsinline', 'webkit-playsinline');
+  video.play();
+  return video;
 }
 
 module.exports = VideoLoader;
