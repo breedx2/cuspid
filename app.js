21f653f566cab16aeac2de81ee9dae0409668188
jason <jason@noisybox.net>
2018-01-06 21:32:02 -0800

    const

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var app = express();
+'use strict';
+
+const express = require('express');
+const app = express();
 
 app.use('/static', express.static(__dirname + '/static', {
 	index: false
