dad3738f987f4c781f1aa41b5db0fb1e5b420f9c
jason <jason@noisybox.net>
2018-01-08 09:37:02 -0800

    add image sequence animation

---
+++ src/anim-image-sequence.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const THREE = require('three');
+
+class ImageSequence {
+
+	constructor( quads, time ){
+		this.quads = quads;
+    this.lastRender = 0;
+		this.time = time;
+		// this.offset = new THREE.Vector2( 0, 0 );
+		// this.zoom = 1.0;
+		// this.position = { x: 0, y: 0};
+	}
+
+	tick(timeMult){
+    console.log(`timeMult ${timeMult} lastRender ${this.lastRender}`);
+    const now = new Date().getTime();
+    if(now - this.lastRender > this.time){
+      this.quads.slice(1).forEach( quad => quad.position.copy(new THREE.Vector3(-1000000, 0, 0.0)));
+      this.quads[0].position.copy(new THREE.Vector3(0, 0, 0.0));
+      this.quads.push( this.quads.shift());
+      this.lastRender = now;
+    }
+  }
+
+  faster(amount){
+    this.time = Math.max(1, this.time - amount);
+    return this;
+  }
+
+  slower(amount){
+    this.time = this.time + amount);
+    return this;
+  }
+
+  setTime(time){
+    this.time = time;
+  }
+
+  static build(quads, time){
+    return new ImageSequence(quads, time);
+  }
+
+}
+
+module.exports = ImageSequence;
