2b68845a058eb2841158c2c270cc9e5433daad53
jason <jason@noisybox.net>
2018-01-08 10:00:24 -0800

    cleanup

--- src/anim-image-sequence.js
+++ src/anim-image-sequence.js
@@ -6,37 +6,37 @@ const DEFAULT_TIME = 500;
 
 class ImageSequence {
 
-	constructor( quads, time ){
-		this.quads = quads;
+  constructor(quads, time) {
+    this.quads = quads;
     this.lastRender = 0;
-		this.time = time;
-	}
+    this.time = time;
+  }
 
-	tick(timeMult){
+  tick(timeMult) {
     const now = new Date().getTime();
-    if(now - this.lastRender > this.time){
-      this.quads.slice(1).forEach( quad => quad.position.copy(new THREE.Vector3(-1000000, 0, 0.0)));
+    if (now - this.lastRender > this.time) {
+      this.quads.slice(1).forEach(quad => quad.position.copy(new THREE.Vector3(-1000000, 0, 0.0)));
       this.quads[0].position.copy(new THREE.Vector3(0, 0, 0.0));
-      this.quads.push( this.quads.shift());
+      this.quads.push(this.quads.shift());
       this.lastRender = now;
     }
   }
 
-  faster(amount){
+  faster(amount) {
     this.time = Math.max(1, this.time - amount);
     return this;
   }
 
-  slower(amount){
+  slower(amount) {
     this.time = this.time + amount;
     return this;
   }
 
-  setTime(time){
+  setTime(time) {
     this.time = time;
   }
 
-  static build(quads, time = DEFAULT_TIME){
+  static build(quads, time = DEFAULT_TIME) {
     return new ImageSequence(quads, time);
   }
 
