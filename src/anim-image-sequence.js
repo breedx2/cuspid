7498fb40bffed75dc5649c3d7a8f2f26b9499b5c
jason <jason@noisybox.net>
2018-01-09 17:40:24 -0800

    trying default prettier

--- src/anim-image-sequence.js
+++ src/anim-image-sequence.js
@@ -1,21 +1,20 @@
-'use strict';
+"use strict";
 
-const THREE = require('three');
-const zoomNudge = require('./zoom_nudge');
+const THREE = require("three");
+const zoomNudge = require("./zoom_nudge");
 
 const DEFAULT_TIME = 500;
 
 class ImageSequence {
-
   constructor(quads, time) {
     this.quads = quads;
     this.lastSwitch = 0;
     this.time = time;
     this.zoom = 1.0;
-		this.position = {x: 0, y: 0};
+    this.position = { x: 0, y: 0 };
     this.deltaZoom = zoomNudge.deltaZoom.bind(this);
-		this.deltaX = zoomNudge.deltaX.bind(this);
-		this.deltaY = zoomNudge.deltaY.bind(this);
+    this.deltaX = zoomNudge.deltaX.bind(this);
+    this.deltaY = zoomNudge.deltaY.bind(this);
   }
 
   tick(timeMult) {
@@ -24,8 +23,12 @@ class ImageSequence {
       this.quads.push(this.quads.shift());
       this.lastSwitch = now;
     }
-    this.quads.slice(1).forEach(quad => quad.position.copy(new THREE.Vector3(-100, 0, 0.0)));
-    this.quads[0].position.copy(new THREE.Vector3(this.position.x, this.position.y, 0.0));
+    this.quads
+      .slice(1)
+      .forEach(quad => quad.position.copy(new THREE.Vector3(-100, 0, 0.0)));
+    this.quads[0].position.copy(
+      new THREE.Vector3(this.position.x, this.position.y, 0.0)
+    );
     this.quads[0].scale.copy(new THREE.Vector3(this.zoom, this.zoom, 1.0));
   }
 
@@ -46,7 +49,6 @@ class ImageSequence {
   static build(quads, time = DEFAULT_TIME) {
     return new ImageSequence(quads, time);
   }
-
 }
 
 module.exports = ImageSequence;
