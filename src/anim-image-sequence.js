635982ffffbd9567124a64aa23fdcdfd575e7d9a
jason <jason@noisybox.net>
2018-01-09 17:35:54 -0800

    add zoom and nudge to image sequence

--- src/anim-image-sequence.js
+++ src/anim-image-sequence.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const THREE = require('three');
+const zoomNudge = require('./zoom_nudge');
 
 const DEFAULT_TIME = 500;
 
@@ -8,18 +9,24 @@ class ImageSequence {
 
   constructor(quads, time) {
     this.quads = quads;
-    this.lastRender = 0;
+    this.lastSwitch = 0;
     this.time = time;
+    this.zoom = 1.0;
+		this.position = {x: 0, y: 0};
+    this.deltaZoom = zoomNudge.deltaZoom.bind(this);
+		this.deltaX = zoomNudge.deltaX.bind(this);
+		this.deltaY = zoomNudge.deltaY.bind(this);
   }
 
   tick(timeMult) {
     const now = new Date().getTime();
-    if (now - this.lastRender > this.time) {
-      this.quads.slice(1).forEach(quad => quad.position.copy(new THREE.Vector3(-1000000, 0, 0.0)));
-      this.quads[0].position.copy(new THREE.Vector3(0, 0, 0.0));
+    if (now - this.lastSwitch > this.time) {
       this.quads.push(this.quads.shift());
-      this.lastRender = now;
+      this.lastSwitch = now;
     }
+    this.quads.slice(1).forEach(quad => quad.position.copy(new THREE.Vector3(-100, 0, 0.0)));
+    this.quads[0].position.copy(new THREE.Vector3(this.position.x, this.position.y, 0.0));
+    this.quads[0].scale.copy(new THREE.Vector3(this.zoom, this.zoom, 1.0));
   }
 
   faster(amount) {
