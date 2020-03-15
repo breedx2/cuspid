98d927d83f0b2ba8dbabc3e8d66b9c2c1f2e1cfa
jason <jason@noisybox.net>
2020-03-14 17:41:24 -0700

    start of tab switching

--- src/ui/imagepool.js
+++ src/ui/imagepool.js
@@ -7,8 +7,32 @@ const _ = require('lodash');
 function setup(useTheseImagesCallback){
   console.log('Performing imagepool setup');
   dropsite.setup(useTheseImagesCallback);
+  setupTabHandlers();
 }
 
+function setupTabHandlers(){
+  _.range(1,10).forEach(n => {
+    const tab = document.getElementById(`settab${n}`);
+    tab.onclick = tabClicked(n);
+  });
+}
+
+function tabClicked(num){
+  return () => {
+    _.range(1,10).filter(n => n != num).forEach(n => unselectTab(n));
+    selectTab(num);
+  };
+}
+
+function selectTab(num){
+  const tab = document.getElementById(`settab${num}`);
+  tab.classList.add("selected");
+}
+
+function unselectTab(num){
+  const tab = document.getElementById(`settab${num}`);
+  tab.classList.remove("selected");
+}
 
 module.exports = {
   setup
