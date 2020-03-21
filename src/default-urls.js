0aed59b0737d1a6d1e4f4a88c1d1f6f8705ae8a4
jason <jason@noisybox.net>
2020-03-21 15:43:44 -0700

    whoops, forgot add

---
+++ src/default-urls.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// These define the default sets for slots 1 & 2
+
+const URLS1 = [
+	'/static/set1/cuspid.jpg', '/static/set1/tornado_carnage.jpg', '/static/set1/needle_things.jpg',
+	'/static/set1/cows01.jpg', '/static/set1/bloody20sunday.jpg', '/static/set1/surgical_implements.jpg',
+	'/static/set1/winter_trees.mp4'
+];
+const xURLS1 = [	'/static/set1/cuspid.jpg'];
+
+const URLS2 = [
+	'/static/set2/z447p782.jpg',	'/static/set2/z46my5u6.jpg',
+	'/static/set2/z8jhy9hw.jpg',	'/static/set2/z9n6pe2b.jpg',
+	'/static/set2/z9td69vw.jpg',	'/static/set2/zfd2e6md.jpg',
+	'/static/set2/zmuqxzc3.jpg',	'/static/set2/zv4322cb.jpg'
+];
+const xURLS2 = [ '/static/set2/z447p782.jpg' ];
+
+
+module.exports = {
+  URLS1,
+  URLS2
+};
